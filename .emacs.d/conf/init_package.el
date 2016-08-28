84a8e885e654ebe2e9713a959ee1ac14e92a9fbe
sfujiwara <shuhei.fujiwara@gmail.com>
2016-08-29 00:05:51 +0900

    package.el 用の設定を修正

--- .emacs.d/conf/init_package.el
+++ .emacs.d/conf/init_package.el
@@ -1,6 +1,5 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 ;; auto install

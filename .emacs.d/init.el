180d346a53688346300e0ee14a0c44386c965875
sfujiwara <shuhei.fujiwara@gmail.com>
2019-02-07 05:54:50 +0900

    init loader

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,29 +1,9 @@
-(setq load-path (append '("~/.emacs.d/conf") load-path))
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
 
-;; より下に記述した物が PATH の先頭に追加されます
-(dolist (dir (list
-              "/sbin"
-              "/usr/sbin"
-              "/bin"
-              "/usr/bin"
-              "/opt/local/bin"
-              "/sw/bin"
-              "/usr/local/bin"
-              "/Library/TeX/texbin"
-              (expand-file-name "~/bin")
-              (expand-file-name "~/.emacs.d/bin")
-              ))
- ;; PATH と exec-path に同じ物を追加します
- (when (and (file-exists-p dir) (not (member dir exec-path)))
-   (setenv "PATH" (concat dir ":" (getenv "PATH")))
-   (setq exec-path (append (list dir) exec-path))))
+(add-to-list 'load-path "~/.emacs.d/elisp")
 
-
-;; Load configuration files
-(load "init_package")
-(load "init_common")
-(load "init_color")
-(load "init_yatex")
-(load "init_aspell")
-(load "init_tabbar")
-(load "init_auto-complete")
+(require 'init-loader)
+(init-loader-load "~/.emacs.d/inits/")

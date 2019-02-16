d476ecbcebc1cfb9b9000c81487777b72953f3d7
sfujiwara <shuhei.fujiwara@gmail.com>
2019-02-17 04:57:31 +0900

    emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,22 +1,70 @@
-;; settings for package.el
 (require 'package)
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
 (package-initialize)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-;; (add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
-;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
-;; refresh if needed
-(unless package-archive-contents (package-refresh-contents))
+;;;;; add melpa and orgmode for packages
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+    ("melpa" . "http://melpa.org/packages/")
+    ("org" . "http://orgmode.org/elpa/")))
 
-;; install init-loader
-(package-install 'init-loader)
+(unless package-archive-contents
+  (package-refresh-contents))
+;;;;; ensure to use use-package
+(when (not (package-installed-p 'use-package))
+  (package-install 'use-package))
+(require 'use-package)
 
-;; init-loader
-(require 'init-loader)
-(setq init-loader-show-log-after-init nil)
-(init-loader-load "~/.emacs.d/inits")
+;;; all-the-icons-dired
+(use-package all-the-icons-dired :quelpa t
+  :defer t ; M-x all-the-icons-install-fonts
+  :config
+  ;;; font-lock+
+  (use-package font-lock+ :quelpa (font-lock+ :fetcher wiki))
+  ;; :hook (dired-mode . all-the-icons-dired-mode)
+  )
 
-;; avoid to write package-selected-packages in init.el
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file) (load custom-file))
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'vscode)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))
+
+(defun sidebar-toggle ()
+  "Toggle both `dired-sidebar' and `ibuffer-sidebar'."
+  (interactive)
+  (dired-sidebar-toggle-sidebar)
+  (ibuffer-sidebar-toggle-sidebar))
+
+;;;;; init-loader
+(use-package init-loader
+  :ensure t
+  :config
+  (init-loader-load "~/.emacs.d/inits"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (init-loader use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

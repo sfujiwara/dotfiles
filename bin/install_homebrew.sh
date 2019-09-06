fae773bf61a73e39e4db4573a25d7c5e427c7ff6
sfujiwara <shuhei.fujiwara@gmail.com>
2019-09-07 01:30:55 +0900

    add permissions

--- bin/install_homebrew.sh
+++ bin/install_homebrew.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -u
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 # TODO: Automate kyeboard inputs
-# echo | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"

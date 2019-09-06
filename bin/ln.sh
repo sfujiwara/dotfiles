8aca10b91ebd78a4520611d41d2411a8f57dcc45
sfujiwara <shuhei.fujiwara@gmail.com>
2019-09-06 17:54:19 +0900

    add comments

--- bin/ln.sh
+++ bin/ln.sh
@@ -3,4 +3,7 @@
 ln -s ~/dotfiles/.zshrc ~/
 ln -s ~/dotfiles/.emacs.d ~/
 ln -s ~/dotfiles/.gitconfig ~/
+
+# TODO: Create directory if not exists
+# TODO: Delete karabiner.json if exists
 ln -s ~/dotfiles/.config/karabiner/karabiner.json ~/.config/karabiner/

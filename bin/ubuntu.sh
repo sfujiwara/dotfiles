ac097d8d4b319ef4fe63ab3068f696ad113ee044
sfujiwara <shuhei.fujiwara@gmail.com>
2021-07-14 04:29:30 +0900

    hello

--- bin/ubuntu.sh
+++ bin/ubuntu.sh
@@ -1,5 +1,9 @@
-# Change default directory names from JP to EN
+# Change default directory names from JP to EN.
 env LANGUAGE=C LC_MESSAGES=C xdg-user-dirs-gtk-update
 
-# Install pyenv
-git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+./bin/apt.sh
+
+# Install pyenv.
+if [ ! -d "$HOME/.pyenv" ]; then
+  git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+fi

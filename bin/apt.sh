12db17aff6374457619676608daff40b57c38d79
sfujiwara <shuhei.fujiwara@gmail.com>
2021-07-14 23:29:45 +0900

    install google chrome via apt

--- bin/apt.sh
+++ bin/apt.sh
@@ -7,3 +7,17 @@ sudo apt -y install docker
 sudo apt -y install docker-compose
 sudo apt -y install cowsay
 sudo apt -y install sl
+
+# Install Google Chrome.
+# curl https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+# echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+sudo apt update
+sudo apt install google-chrome-stable
+
+
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo apt install ./google-chrome-stable_current_amd64.deb

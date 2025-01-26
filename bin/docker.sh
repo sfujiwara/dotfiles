bb1408d08360b040a01fe8ded893231b569d9556
sfujiwara <shuhei.fujiwara@gmail.com>
2025-01-27 05:25:32 +0900

    add docker docs url

--- bin/docker.sh
+++ bin/docker.sh
@@ -1,3 +1,4 @@
+# https://docs.docker.com/engine/install/ubuntu/
 
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc

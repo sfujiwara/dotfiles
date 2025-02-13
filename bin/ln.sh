3ef8040effed3c501e60c888110dc125bbe5680a
sfujiwara <shuhei.fujiwara@gmail.com>
2025-02-14 07:16:59 +0900

    symlink

--- bin/ln.sh
+++ bin/ln.sh
@@ -20,10 +20,12 @@ if [ `uname` = "Darwin" ]; then
   mkdir -p ~/.config/karabiner
   ln -nfs ~/dotfiles/karabiner.json ~/.config/karabiner/
   # Visual Studio Code.
-  # ln -nfs ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/
+  ln -nfs ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+  ln -nfs ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 fi
 
 if [ `uname` = "Linux" ]; then
   # Visual Studio Code.
   ln -nfs $HOME/dotfiles/vscode/settings.json $HOME/.config/Code/User/settings.json
+  ln -nfs $HOME/dotfiles/vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
 fi

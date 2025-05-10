c3f034de6d12b1e4fe011af4cc60375c4027d891
sfujiwara <shuhei.fujiwara@gmail.com>
2025-05-11 02:44:46 +0900

    symlink for latexmk

--- bin/ln.sh
+++ bin/ln.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-# zsh.
+# zsh
 ln -nfs ~/dotfiles/.zshrc ~/
 
-# bash.
+# bash
 ln -nfs ~/dotfiles/.bashrc $HOME/
 
-# Emacs.
+# Emacs
 ln -nfs ~/dotfiles/.emacs.d ~/
 
-# Git.
+# Git
 ln -nfs ~/dotfiles/.gitconfig ~/
 mkdir -p ~/.config/git
 ln -nfs ~/dotfiles/.config/git/ignore ~/.config/git/
 
+# LaTeX
+ln -nfs ${HOME}/dotfiles/.latexmkrc ${HOME}/.latexmkrc
+
 # tmux
 ln -nfs ~/dotfiles/.tmux.conf ~/
 
-# Settings for Mac.
+# Settings for Mac
 if [ `uname` = "Darwin" ]; then
-  # Karabiner.
+  # Karabiner
   mkdir -p ~/.config
   ln -nfs ~/dotfiles/.config/karabiner ~/.config/
-  # Visual Studio Code.
+  # Visual Studio Code
   ln -nfs ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
   ln -nfs ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 fi
 
 if [ `uname` = "Linux" ]; then
-  # Visual Studio Code.
+  # Visual Studio Code
   ln -nfs $HOME/dotfiles/vscode/settings.json $HOME/.config/Code/User/settings.json
   ln -nfs $HOME/dotfiles/vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
 fi

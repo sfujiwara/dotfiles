72fdb188daf504895ae3e6115132ba76836a954b
sfujiwara <shuhei.fujiwara@gmail.com>
2025-07-06 03:19:14 +0900

    apt

--- bin/ubuntu/apt.sh
+++ bin/ubuntu/apt.sh
@@ -9,6 +9,9 @@ sudo apt -y install direnv
 sudo apt -y install fcitx5-mozc
 sudo apt -y install zsh
 sudo apt -y install gnome-tweaks
+sudo apt -y install pipx
+sudo apt -y install python3-pylsp
+sudo apt -y install tmux
 sudo apt -y install build-essential
 sudo apt -y install libbz2-dev libdb-dev libreadline-dev libffi-dev libgdbm-dev liblzma-dev libncursesw5-dev libsqlite3-dev libssl-dev zlib1g-dev uuid-dev tk-dev python3-dev
 sudo apt -y install libfuse2  # Required to run JetBrains Toolbox.

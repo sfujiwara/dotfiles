56d5c28995c6e41bcd65fd457c5401d9bf7379e2
sfujiwara <shuhei.fujiwara@gmail.com>
2025-08-23 18:00:29 +0900

    add script to install hackgen font

--- bin/ubuntu/install_hackgen.sh
+++ bin/ubuntu/install_hackgen.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -e
 
 VERSION=v2.10.0
-HACKGEN=HackGen_$VERSION
-HACKGEN_NF=HackGen_NF_$VERSION
+HACKGEN=HackGen_${VERSION}
+HACKGEN_NF=HackGen_NF_${VERSION}
 
 mkdir -p ${HOME}/.local/share/fonts
 
 # Install HackGen
 wget https://github.com/yuru7/HackGen/releases/download/${VERSION}/${HACKGEN}.zip
-unzip  -d ./ ./$HACKGEN
+unzip  -d ./ ./${HACKGEN}
 cp ./${HACKGEN}/*.ttf ${HOME}/.local/share/fonts/
 rm ${HACKGEN}.zip
 rm -r ${HACKGEN}
 
 # Install HackGen NF
 wget https://github.com/yuru7/HackGen/releases/download/${VERSION}/${HACKGEN_NF}.zip
-unzip  -d ./ ./$HACKGEN_NF
+unzip  -d ./ ./${HACKGEN_NF}
 cp ./${HACKGEN_NF}/*.ttf ${HOME}/.local/share/fonts/
 rm ${HACKGEN_NF}.zip
 rm -r ${HACKGEN_NF}
